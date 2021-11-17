21c207eec627e8478572f253bf8d5c25be9de925
Sumanth <sumanth.l@testyantra.com>
2021-11-17 10:42:32 +0530

    Added BaseClass and updated Dummy class

---
+++ oneBill/src/main/java/genericLib/BaseClass.java
@@ -0,0 +1,7 @@
+package genericLib;
+
+import org.testng.annotations.Test;
+
+public class BaseClass {
+
+}
--- oneBill/src/test/java/oneBill/Dummy.java
+++ oneBill/src/test/java/oneBill/Dummy.java
@@ -2,13 +2,19 @@ package oneBill;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Dummy {
 
-	public static void main(String[] args) {
+	@Test
+	public void demo() {
+		Assert.fail();
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		
 	}
 
 }
