06d086677542b104f53db677a220c544f1864666
Sumanth <sumanth.l@testyantra.com>
2021-11-17 10:29:04 +0530

    Pushing framework of OneBill project

---
+++ oneBill/src/test/java/oneBill/Dummy.java
@@ -0,0 +1,14 @@
+package oneBill;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
